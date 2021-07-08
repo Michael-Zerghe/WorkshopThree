243a5a45743049a25bd498df95c1210a20c28722
Patricia <patricia.corduant@gmail.com>
2021-07-08 11:21:05 +0200

    fils to start

---
+++ main.js
@@ -0,0 +1,38 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.130.1';
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
+
+//declarer une scene
+
+
+//declarer le moteur de rendu 
+//+ mettre le rendu du moteur de rendu en plein écran
+//et l’ajouter dans la page HTML via le canvas HTML5!
+
+
+
+//declarer une camera
+
+
+//le controle de la camera (orbitcontrol)... permet de pouvoir bouger l objet dans la scene, pas obligatoire
+
+
+//La forme ou creer le mesh cad l 'objet (geometry, material, mesh)
+
+
+
+
+
+//ajouter la forme a la scene
+
+
+
+//si on veut animer l objet ...
+
+
+
+
+
+
+
+// ou juste l afficher en fixe
+
